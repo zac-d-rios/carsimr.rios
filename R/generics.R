380ab1ee7fa6ba32f173cb07f45f54d2ce15763b
Zac Rios <zacdrios@gmail.com>
2023-02-24 18:42:43 -0700

    Homework 2

--- R/generics.R
+++ R/generics.R
@@ -4,7 +4,7 @@
 
 plot.carsimr <- function(x, y, ...) {
   attr(x, "class") <- NULL
-  image(1:ncol(x), 1:nrow(x), t(x[nrow(x):1, ]),
+  image(seq_len(ncol(x)), seq_len(nrow(x)), t(x[nrow(x):1, ]),
     col = c("gray", "red", "blue"),
     axes = FALSE, xlab = "", ylab = "", breaks = -1:2, ...
   )
@@ -14,7 +14,7 @@ plot.carsimr <- function(x, y, ...) {
 #' @export
 
 plot.carsimr_list <- function(x, y, pause = 1, ...) {
-  for (i in 1:length(x)) {
+  for (i in seq_along(x)) {
     plot(x[[i]], main = paste("Iteration", i - 1, sep = " "))
     Sys.sleep(pause)
   }
